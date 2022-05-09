ce71d40b421e032e3583773dd85517a446fe8ded
jxxtty <jaehee941106@gmail.com>
2022-05-09 16:10:36 +0900

    component practice-2 props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() { // 사용자 정의 태그를 만들때는 항상 대문자로 시작해야한다!(사용자 정의태그 = 컴포넌트)
-    // html 코드를 return
+function Header(props) { // 사용자 정의 태그를 만들때는 항상 대문자로 시작해야한다!(사용자 정의태그 = 컴포넌트)
+    // {} 중괄호로 감싸면 표현식으로 인식되어서 props.title이 출력되는게아니라 그에 담긴 값인 "REACT"가 출력된다.
     return  <header>
-                <h1><a href={"/"}>WEB</a></h1>
+                <h1><a href={"/"}>{props.title}</a></h1>
             </header>
 }
 
-function Nav() {
+function Nav(props) {
+    const lis = [
+        <li><a href={"/read/1"}>html</a> </li>,
+        <li><a href={"/read/2"}>css</a> </li>,
+        <li><a href={"/read/3"}>js</a> </li>
+    ]
+    const lis2 = []
+    for(let i = 0 ; i < props.topics.length; i++) {
+        let t = props.topics[i]
+        lis2.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    }
     return  <nav>
                 <ol>
-                    <li><a href={"/read/1"}>html</a> </li>
-                    <li><a href={"/read/2"}>css</a> </li>
-                    <li><a href={"/read/3"}>js</a> </li>
+                    {lis}
+                </ol>
+                <ol>
+                    {lis2}
                 </ol>
             </nav>
 }
 
-function Article() {
+function Article(props) {
     return  <article>
-                <h2>Welcome</h2>
-                Hello, WEB
+                <h2>{props.title}</h2>
+                {props.body}
             </article>
 }
 
 function App() {
-  return (
-    <div>
-        <Header></Header>
+    const topics = [
+        {id:1, title:'html', body:'html is ...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'js', body:'javascript is ...'}
+    ]
+    return (
+        <div>
+            <Header title={"REACT"}></Header>
 
-        <Nav></Nav>
+            <Nav topics={topics}></Nav>
 
-        <Article></Article>
-    </div>
-  );
+            <Article title={"Welcome"} body={"Hello,Web"}></Article>
+            <Article title={"Hi"} body={"Hello,react"}></Article>
+        </div>
+    );
 }
 
 export default App;
