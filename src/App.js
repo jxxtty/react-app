3d28534673ccd984d8ebc0c780ac9398a983b364
jxxtty <jaehee941106@gmail.com>
2022-05-10 10:16:19 +0900

    event

--- src/App.js
+++ src/App.js
@@ -4,25 +4,35 @@ import './App.css';
 function Header(props) { // 사용자 정의 태그를 만들때는 항상 대문자로 시작해야한다!(사용자 정의태그 = 컴포넌트)
     // {} 중괄호로 감싸면 표현식으로 인식되어서 props.title이 출력되는게아니라 그에 담긴 값인 "REACT"가 출력된다.
     return  <header>
-                <h1><a href={"/"}>{props.title}</a></h1>
+                <h1><a href={"/"} onClick={event => {
+                    event.preventDefault();
+                    props.onChangeMode();
+                }}>{props.title}</a></h1>
             </header>
 }
 
 function Nav(props) {
-    const lis = [
-        <li><a href={"/read/1"}>html</a> </li>,
-        <li><a href={"/read/2"}>css</a> </li>,
-        <li><a href={"/read/3"}>js</a> </li>
-    ]
+    // const lis = [
+    //     <li><a href={"/read/1"}>html</a> </li>,
+    //     <li><a href={"/read/2"}>css</a> </li>,
+    //     <li><a href={"/read/3"}>js</a> </li>
+    // ]
+
     const lis2 = []
     for(let i = 0 ; i < props.topics.length; i++) {
         let t = props.topics[i]
-        lis2.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+        lis2.push(<li key={t.id}>
+            <a id={t.id} href={'/read/'+t.id} onClick={event => {
+                    event.preventDefault();
+                    props.onChangeMode(event.target.id);
+                }}>{t.title}
+            </a>
+        </li>)
     }
     return  <nav>
-                <ol>
-                    {lis}
-                </ol>
+                {/*<ol>*/}
+                {/*    {lis}*/}
+                {/*</ol>*/}
                 <ol>
                     {lis2}
                 </ol>
@@ -44,9 +54,13 @@ function App() {
     ]
     return (
         <div>
-            <Header title={"REACT"}></Header>
+            <Header title={"WEB"} onChangeMode={() => {
+                alert('Header');
+            }}></Header>
 
-            <Nav topics={topics}></Nav>
+            <Nav topics={topics} onChangeMode={(id)=>{
+                alert(id);
+            }}></Nav>
 
             <Article title={"Welcome"} body={"Hello,Web"}></Article>
             <Article title={"Hi"} body={"Hello,react"}></Article>
